6813f56e01276fe214d47be4c652c231dcb6a6ad
abc980823 <80378041+abc980823@users.noreply.github.com>
2021-09-07 01:45:27 +0900

    Update report 3-PACK.py

--- Assignment/report 3-PACK.py
+++ Assignment/report 3-PACK.py
@@ -25,19 +25,13 @@ def Big_box(box):                                   # 큰 박스 1개를 만드�
     max_h = box[0][1] + box[1][1] + box[2][1]       # 세로 길이는 3개의 박스를 세로로 붙인 총 길이이고, 가로 길이는 3개의 박스중에 가장 가로로 긴 것이다.
     price_2 = max_w*max_w + max_h*max_h
 
-    max_h = box[0][1]                               # 박스를 3개 모두 한 방향으로 붙이는 경우(가로)
-    for i in range(1,3):
-        if max_h < box[i][1]:
-            max_h = box[i][1]
-    max_w = box[0][0] + box[1][0] + box[2][0]       
-    price_3 = max_w*max_w + max_h*max_h
 
-    least_price = min(price_1,price_2,price_3)      # 만든 3개의 큰 박스 중에 비용이 가장 작은 것을 선택한다(가로^2+세로^2의 값이 작은 것)
+    least_price = min(price_1,price_2)              # 만든 3개의 큰 박스 중에 비용이 가장 작은 것을 선택한다(가로^2+세로^2의 값이 작은 것)
     return least_price
 
 least_price = Big_box(box)                          # 처음 입력한 박스를 돌리지 않고 만들 수 있는 가장 적은 비용의 큰 박스
 
-for i in range(3):                                   # 박스를 돌리는 모든 경우를 해 주었다.
+for i in range(3):                                  # 박스를 돌리는 모든 경우를 해 주었다.
     box[i].reverse()
     for j in range(i+1,3):
         box[j].reverse()
